8b00156b9f9d6d40b0f227b5891a939b2ed4b303
xiaohuijun <huijun.xiao@amesante.com>
2016-08-08 15:46:13 +0800

    webview公用activity

---
+++ app/src/main/java/com/xiaohuijun/administrator/mymvp/ui/module/common/WebView/WebViewActivity.java
@@ -0,0 +1,16 @@
+package com.xiaohuijun.administrator.mymvp.ui.module.common.WebView;
+
+import android.os.Bundle;
+
+import com.xiaohuijun.administrator.mymvp.R;
+import com.xiaohuijun.administrator.mymvp.ui.BaseActivity;
+
+public class WebViewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+    }
+}
